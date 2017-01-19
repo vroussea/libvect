e1bf4a3ef17489769b28f18ff5b86f349a7b4403
Valentin ROUSSEAU <vroussea@e1r10p2.42.fr>
2017-01-19 17:49:38 +0100

    remove printf

--- srcs/vect3d_rotations.c
+++ srcs/vect3d_rotations.c
@@ -6,23 +6,20 @@
 /*   By: vroussea <vroussea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/18 15:16:43 by vroussea          #+#    #+#             */
-/*   Updated: 2017/01/19 16:37:39 by vroussea         ###   ########.fr       */
+/*   Updated: 2017/01/19 17:49:18 by vroussea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libvect3d.h"
 #include <math.h>
-#include <stdio.h>
 
 t_vect3d	vect3d_rot_x(t_vect3d u, double ang)
 {
 	t_vect3d	tmp;
 
 	tmp = u;
-	printf("x : %f\ny : %f\nz : %f\n", u.x, u.y, u.z);
 	u.y = tmp.y * cos(ang) - tmp.z * sin(ang);
 	u.z = tmp.z * cos(ang) + tmp.y * sin(ang);
-	printf("x : %f\ny : %f\nz : %f\n", u.x, u.y, u.z);
 	return (u);
 }
 
@@ -31,10 +28,8 @@ t_vect3d	vect3d_rot_y(t_vect3d u, double ang)
 	t_vect3d	tmp;
 
 	tmp = u;
-	printf("x : %f\ny : %f\nz : %f\n", u.x, u.y, u.z);
 	u.x = tmp.x * cos(ang) + tmp.z * sin(ang);
 	u.z = tmp.z * cos(ang) - tmp.x * sin(ang);
-	printf("x : %f\ny : %f\nz : %f\n", u.x, u.y, u.z);
 	return (u);
 }
 
@@ -43,9 +38,7 @@ t_vect3d	vect3d_rot_z(t_vect3d u, double ang)
 	t_vect3d	tmp;
 
 	tmp = u;
-	printf("x : %f\ny : %f\nz : %f\n", u.x, u.y, u.z);
 	u.x = tmp.x * cos(ang) - tmp.y * sin(ang);
 	u.y = tmp.y * cos(ang) + tmp.x * sin(ang);
-	printf("x : %f\ny : %f\nz : %f\n", u.x, u.y, u.z);
 	return (u);
 }
